e3131b48f4e6a4eb938dfaf7ef0e2b5722afd251
Nicolas Coll <74115272+nicolascoll@users.noreply.github.com>
2021-08-18 19:13:13 -0300

    Update CartWidget.js

--- CartWidget.js
+++ CartWidget.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const CartWidget = () => {
     return (
 
-        <a href="#"><img src="img/shopping-cart.png" style={{height: 24}}/></a>
+        <a href="#"><img src="/shopping-cart.png" style={{height: 24}}/></a>
 
     )
 }
